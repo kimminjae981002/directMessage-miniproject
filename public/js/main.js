320c35252854c05a0d77636115b9bd7838b393ee
minjae Kim <cmg981548@gmail.com>
2024-04-11 22:11:32 +0900

    feat: [main] 채팅방 접속 시 유저 소케 연결 완료 및 채팅방 입장 구현

--- public/js/main.js
+++ public/js/main.js
@@ -16,4 +16,85 @@ const userTable = document.querySelector(".users");
 const userTagline = document.querySelector("#users-tagline");
 const title = document.querySelector("#active-user");
 const messages = document.querySelector(".messages");
-const msgDiv = document.querySelector("msg-form");
+const msgDiv = document.querySelector(".msg-form");
+
+// 유저가 로그인 입력했을 때 세션에 저장
+const loginForm = document.querySelector(".user-login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // 새로고침 막음
+  const username = document.getElementById("username");
+  createSession(username.value.toLowerCase());
+  username.value = ""; // input 빈값
+});
+
+// 세션에 유저 저장 및 HTML 보여주기
+const createSession = async (username) => {
+  let options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username }),
+  };
+  // 서버에서 세션 데이터 받아오기 (app.post('/session'))
+  await fetch("/session", options)
+    .then((res) => res.json())
+    .then((data) => {
+      // 원래는 자동 connect를 false로 해놔서 유저가 입장할 때만 연결 시킨다.
+      socketConnect(data.username, data.userID);
+
+      localStorage.setItem("session-username", data.username);
+      localStorage.setItem("session-userID", data.userID);
+
+      loginContainer.classList.add("d-none");
+      chatBody.classList.remove("d-none");
+      userTitle.innerHTML = data.username;
+    })
+    .catch((err) => console.log(err));
+};
+
+const socketConnect = async (username, userID) => {
+  socket.auth = { username, userID };
+  // 원래는 자동 connect를 false로 해놔서 유저가 입장할 때만 연결 시킨다.
+  await socket.connect();
+};
+
+// 서버에서 입장 된 유저들을 받아옴
+socket.on("users-data", ({ users }) => {
+  // 본인 제거
+  const index = users.findIndex((user) => user.userID == socket.id);
+  // -1이 아니라면 존재함
+  if (index > -1) {
+    // 내 위치에서 한개를 지움
+    users.splice(index, 1);
+  }
+
+  // 생성하기 user table list
+  let ul = `<table class="table table-hover">`;
+  for (let user of users) {
+    // 동적으로 만듦
+    ul += `<tr class="socket-users" onclick="setActiveUser(this,'${user.username}', '${user.userID}')">
+    <td>${user.username}<span class="text-danger ps-1 d-none" id="${user.userID}">!</span></td>
+            </tr>
+    `;
+  }
+  ul += `</table>`;
+  if (users.length > 0) {
+    userTagline.innerHTML = "접속 중인 유저";
+    userTagline.classList.remove("text-danger");
+    userTagline.classList.add("text-success");
+  } else {
+    userTagline.innerHTML = "접속 중인 유저 없음";
+    userTagline.classList.remove("text-success");
+    userTagline.classList.add("text-danger");
+  }
+});
+
+const sessionUsername = localStorage.getItem("session-username");
+const sessionUserID = localStorage.getItem("session-userID");
+
+if (sessionUsername && sessionUserID) {
+  socketConnect(sessionUsername, sessionUserID);
+
+  loginContainer.classList.add("d-none");
+  chatBody.classList.remove("d-none");
+  userTitle.innerHTML = sessionUsername;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 require("dotenv").config();
 
@@ -25,12 +26,38 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
+// 유저 세션 데이타  클라이언트와 연결
+app.post("/session", (req, res) => {
+  let data = {
+    username: req.body.username,
+    userID: randomID(),
+  };
+  res.send(data);
+});
+const randomID = () => {
+  return crypto.randomBytes(8).toString("hex");
+};
+
+// 소켓 미들웨어
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  const userID = socket.handshake.auth.userID;
+  if (!username) {
+    return next(new Error("Invalid username"));
+  }
+  // 클라이언트 socketConnect()
+  // socket connection 할 때 socket 데이터에서 받아올 수 있다. (io.on(connection))
+  socket.username = username;
+  socket.id = userID;
+  next();
+});
+
 // 유저를 담을 곳
 let users = [];
 // socket 전체 연동
 io.on("connection", async (socket) => {
-  // 유저의 데이타를 객체로 users에 담음
-  let userData = {};
+  // 연결된 사람들만 미들웨어에서 데이터 받아옴
+  let userData = { username: socket.username, userID: socket.id };
   users.push(userData);
   // socket room에 보냄
   io.emit("users-data", { users });
